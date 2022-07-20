295ce1eddb0da2ab44c0debab7982bd6ec912f46
Franklinjr02 <101658484+Franklinjr02@users.noreply.github.com>
2022-07-20 06:04:42 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,32 +1,48 @@
 #!/usr/bin/python3
-"""Define a class Square."""
-
-
 class Square:
-    """Represent a square."""
+    """Test"""
+    def __init__(self, size=0, position=(0, 0)):
+            self.size = size
+            self.position = position
 
-    def __init__(self, size):
-        """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
-        """
-        self.size = size
+    def area(self):
+        return self.__size ** 2
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
+        return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if ((len(value) != 2) or
+            (type(value) is not tuple) or
+            (type(value[0]) is not int) or (type(value[1]) is not int) or
+                (value[0] < 0) or (value[1] < 0)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def my_print(self):
+        if self.__size is 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for j in range(self.__size + self.__position[0]):
+                    if j < self.__position[0]:
+                        print(" ", end='')
+                    else:
+                        print("#", end='')
+                print()
